f265e2d88b756bdfdc5bbdf20c537b2ff13c9dfb
YoungBirds134 <Huynguyen2913@gmail.com>
2021-11-30 13:49:15 +0700

    - create router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TodoHuyComponent } from './todo-huy/todo-huy.component';
+import { TodoToanComponent } from './todo-toan/todo-toan.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'huy', component: TodoHuyComponent},
+  {path:'toan', component: TodoToanComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
